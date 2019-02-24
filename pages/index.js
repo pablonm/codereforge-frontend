403927fafb83a9f683d49c9f64e458ec79dbbac4
Pablo Miceli <pablomiceli24@gmail.com>
2019-02-23 22:36:21 -0300

    husky test

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 
 const Home = () => {
-  console.log('props f:', props)
+  // console.log('props f:', props)
   return <div>Welcome to Jami!</div>
 }
 
