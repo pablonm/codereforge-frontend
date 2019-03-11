26af946fdf8ff5abf0e7e61f796306232a11f18e
Pablo Miceli <pablomiceli24@gmail.com>
2019-03-11 20:09:20 -0300

    code editor bug

--- components/Common/CodeEditor/CodeEditor.js
+++ components/Common/CodeEditor/CodeEditor.js
@@ -1,13 +1,132 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
+import * as CodeMirror from 'react-codemirror2'
 import { Row, Col } from 'antd'
 import Select from 'uikit/Select'
 import { EditorContainer, ErrorMessage } from './CodeEditorStyle'
 
-require('codemirror/lib/codemirror.css')
-require('codemirror/theme/darcula.css')
-
-let CodeMirror
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/darcula.css'
+import 'codemirror/mode/apl/apl'
+import 'codemirror/mode/asciiarmor/asciiarmor'
+import 'codemirror/mode/asn.1/asn.1'
+import 'codemirror/mode/asterisk/asterisk'
+import 'codemirror/mode/brainfuck/brainfuck'
+import 'codemirror/mode/clike/clike'
+import 'codemirror/mode/clojure/clojure'
+import 'codemirror/mode/cmake/cmake'
+import 'codemirror/mode/cobol/cobol'
+import 'codemirror/mode/coffeescript/coffeescript'
+import 'codemirror/mode/commonlisp/commonlisp'
+import 'codemirror/mode/crystal/crystal'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/cypher/cypher'
+import 'codemirror/mode/d/d'
+import 'codemirror/mode/dart/dart'
+import 'codemirror/mode/diff/diff'
+import 'codemirror/mode/django/django'
+import 'codemirror/mode/dockerfile/dockerfile'
+import 'codemirror/mode/dtd/dtd'
+import 'codemirror/mode/dylan/dylan'
+import 'codemirror/mode/ebnf/ebnf'
+import 'codemirror/mode/ecl/ecl'
+import 'codemirror/mode/eiffel/eiffel'
+import 'codemirror/mode/elm/elm'
+import 'codemirror/mode/erlang/erlang'
+import 'codemirror/mode/factor/factor'
+import 'codemirror/mode/fcl/fcl'
+import 'codemirror/mode/forth/forth'
+import 'codemirror/mode/fortran/fortran'
+import 'codemirror/mode/gas/gas'
+import 'codemirror/mode/gfm/gfm'
+import 'codemirror/mode/gherkin/gherkin'
+import 'codemirror/mode/go/go'
+import 'codemirror/mode/groovy/groovy'
+import 'codemirror/mode/haml/haml'
+import 'codemirror/mode/handlebars/handlebars'
+import 'codemirror/mode/haskell/haskell'
+import 'codemirror/mode/haskell-literate/haskell-literate'
+import 'codemirror/mode/haxe/haxe'
+import 'codemirror/mode/htmlembedded/htmlembedded'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/mode/http/http'
+import 'codemirror/mode/idl/idl'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/jinja2/jinja2'
+import 'codemirror/mode/jsx/jsx'
+import 'codemirror/mode/julia/julia'
+import 'codemirror/mode/livescript/livescript'
+import 'codemirror/mode/lua/lua'
+import 'codemirror/mode/markdown/markdown'
+import 'codemirror/mode/mathematica/mathematica'
+import 'codemirror/mode/mbox/mbox'
+import 'codemirror/mode/mirc/mirc'
+import 'codemirror/mode/mllike/mllike'
+import 'codemirror/mode/modelica/modelica'
+import 'codemirror/mode/mscgen/mscgen'
+import 'codemirror/mode/mumps/mumps'
+import 'codemirror/mode/nginx/nginx'
+import 'codemirror/mode/nsis/nsis'
+import 'codemirror/mode/ntriples/ntriples'
+import 'codemirror/mode/octave/octave'
+import 'codemirror/mode/oz/oz'
+import 'codemirror/mode/pascal/pascal'
+import 'codemirror/mode/pegjs/pegjs'
+import 'codemirror/mode/perl/perl'
+import 'codemirror/mode/php/php'
+import 'codemirror/mode/pig/pig'
+import 'codemirror/mode/powershell/powershell'
+import 'codemirror/mode/properties/properties'
+import 'codemirror/mode/protobuf/protobuf'
+import 'codemirror/mode/pug/pug'
+import 'codemirror/mode/puppet/puppet'
+import 'codemirror/mode/python/python'
+import 'codemirror/mode/q/q'
+import 'codemirror/mode/r/r'
+import 'codemirror/mode/rpm/rpm'
+import 'codemirror/mode/rst/rst'
+import 'codemirror/mode/ruby/ruby'
+import 'codemirror/mode/rust/rust'
+import 'codemirror/mode/sas/sas'
+import 'codemirror/mode/sass/sass'
+import 'codemirror/mode/scheme/scheme'
+import 'codemirror/mode/shell/shell'
+import 'codemirror/mode/sieve/sieve'
+import 'codemirror/mode/slim/slim'
+import 'codemirror/mode/smalltalk/smalltalk'
+import 'codemirror/mode/smarty/smarty'
+import 'codemirror/mode/solr/solr'
+import 'codemirror/mode/soy/soy'
+import 'codemirror/mode/sparql/sparql'
+import 'codemirror/mode/spreadsheet/spreadsheet'
+import 'codemirror/mode/sql/sql'
+import 'codemirror/mode/stex/stex'
+import 'codemirror/mode/stylus/stylus'
+import 'codemirror/mode/swift/swift'
+import 'codemirror/mode/tcl/tcl'
+import 'codemirror/mode/textile/textile'
+import 'codemirror/mode/tiddlywiki/tiddlywiki'
+import 'codemirror/mode/tiki/tiki'
+import 'codemirror/mode/toml/toml'
+import 'codemirror/mode/tornado/tornado'
+import 'codemirror/mode/troff/troff'
+import 'codemirror/mode/ttcn/ttcn'
+import 'codemirror/mode/ttcn-cfg/ttcn-cfg'
+import 'codemirror/mode/turtle/turtle'
+import 'codemirror/mode/twig/twig'
+import 'codemirror/mode/vb/vb'
+import 'codemirror/mode/vbscript/vbscript'
+import 'codemirror/mode/velocity/velocity'
+import 'codemirror/mode/verilog/verilog'
+import 'codemirror/mode/vhdl/vhdl'
+import 'codemirror/mode/vue/vue'
+import 'codemirror/mode/webidl/webidl'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/xquery/xquery'
+import 'codemirror/mode/yacas/yacas'
+import 'codemirror/mode/yaml/yaml'
+import 'codemirror/mode/yaml-frontmatter/yaml-frontmatter'
+import 'codemirror/mode/z80/z80'
 
 const modes = [
   'apl',
@@ -134,134 +253,12 @@ const modes = [
 
 class CodeEditor extends Component {
   state = {
-    render: false,
     selectedMode: null,
   }
 
   componentDidMount = () => {
-    CodeMirror = require('react-codemirror2')
-    require('codemirror/mode/apl/apl')
-    require('codemirror/mode/asciiarmor/asciiarmor')
-    require('codemirror/mode/asn.1/asn.1')
-    require('codemirror/mode/asterisk/asterisk')
-    require('codemirror/mode/brainfuck/brainfuck')
-    require('codemirror/mode/clike/clike')
-    require('codemirror/mode/clojure/clojure')
-    require('codemirror/mode/cmake/cmake')
-    require('codemirror/mode/cobol/cobol')
-    require('codemirror/mode/coffeescript/coffeescript')
-    require('codemirror/mode/commonlisp/commonlisp')
-    require('codemirror/mode/crystal/crystal')
-    require('codemirror/mode/css/css')
-    require('codemirror/mode/cypher/cypher')
-    require('codemirror/mode/d/d')
-    require('codemirror/mode/dart/dart')
-    require('codemirror/mode/diff/diff')
-    require('codemirror/mode/django/django')
-    require('codemirror/mode/dockerfile/dockerfile')
-    require('codemirror/mode/dtd/dtd')
-    require('codemirror/mode/dylan/dylan')
-    require('codemirror/mode/ebnf/ebnf')
-    require('codemirror/mode/ecl/ecl')
-    require('codemirror/mode/eiffel/eiffel')
-    require('codemirror/mode/elm/elm')
-    require('codemirror/mode/erlang/erlang')
-    require('codemirror/mode/factor/factor')
-    require('codemirror/mode/fcl/fcl')
-    require('codemirror/mode/forth/forth')
-    require('codemirror/mode/fortran/fortran')
-    require('codemirror/mode/gas/gas')
-    require('codemirror/mode/gfm/gfm')
-    require('codemirror/mode/gherkin/gherkin')
-    require('codemirror/mode/go/go')
-    require('codemirror/mode/groovy/groovy')
-    require('codemirror/mode/haml/haml')
-    require('codemirror/mode/handlebars/handlebars')
-    require('codemirror/mode/haskell/haskell')
-    require('codemirror/mode/haskell-literate/haskell-literate')
-    require('codemirror/mode/haxe/haxe')
-    require('codemirror/mode/htmlembedded/htmlembedded')
-    require('codemirror/mode/htmlmixed/htmlmixed')
-    require('codemirror/mode/http/http')
-    require('codemirror/mode/idl/idl')
-    require('codemirror/mode/javascript/javascript')
-    require('codemirror/mode/jinja2/jinja2')
-    require('codemirror/mode/jsx/jsx')
-    require('codemirror/mode/julia/julia')
-    require('codemirror/mode/livescript/livescript')
-    require('codemirror/mode/lua/lua')
-    require('codemirror/mode/markdown/markdown')
-    require('codemirror/mode/mathematica/mathematica')
-    require('codemirror/mode/mbox/mbox')
-    require('codemirror/mode/mirc/mirc')
-    require('codemirror/mode/mllike/mllike')
-    require('codemirror/mode/modelica/modelica')
-    require('codemirror/mode/mscgen/mscgen')
-    require('codemirror/mode/mumps/mumps')
-    require('codemirror/mode/nginx/nginx')
-    require('codemirror/mode/nsis/nsis')
-    require('codemirror/mode/ntriples/ntriples')
-    require('codemirror/mode/octave/octave')
-    require('codemirror/mode/oz/oz')
-    require('codemirror/mode/pascal/pascal')
-    require('codemirror/mode/pegjs/pegjs')
-    require('codemirror/mode/perl/perl')
-    require('codemirror/mode/php/php')
-    require('codemirror/mode/pig/pig')
-    require('codemirror/mode/powershell/powershell')
-    require('codemirror/mode/properties/properties')
-    require('codemirror/mode/protobuf/protobuf')
-    require('codemirror/mode/pug/pug')
-    require('codemirror/mode/puppet/puppet')
-    require('codemirror/mode/python/python')
-    require('codemirror/mode/q/q')
-    require('codemirror/mode/r/r')
-    require('codemirror/mode/rpm/rpm')
-    require('codemirror/mode/rst/rst')
-    require('codemirror/mode/ruby/ruby')
-    require('codemirror/mode/rust/rust')
-    require('codemirror/mode/sas/sas')
-    require('codemirror/mode/sass/sass')
-    require('codemirror/mode/scheme/scheme')
-    require('codemirror/mode/shell/shell')
-    require('codemirror/mode/sieve/sieve')
-    require('codemirror/mode/slim/slim')
-    require('codemirror/mode/smalltalk/smalltalk')
-    require('codemirror/mode/smarty/smarty')
-    require('codemirror/mode/solr/solr')
-    require('codemirror/mode/soy/soy')
-    require('codemirror/mode/sparql/sparql')
-    require('codemirror/mode/spreadsheet/spreadsheet')
-    require('codemirror/mode/sql/sql')
-    require('codemirror/mode/stex/stex')
-    require('codemirror/mode/stylus/stylus')
-    require('codemirror/mode/swift/swift')
-    require('codemirror/mode/tcl/tcl')
-    require('codemirror/mode/textile/textile')
-    require('codemirror/mode/tiddlywiki/tiddlywiki')
-    require('codemirror/mode/tiki/tiki')
-    require('codemirror/mode/toml/toml')
-    require('codemirror/mode/tornado/tornado')
-    require('codemirror/mode/troff/troff')
-    require('codemirror/mode/ttcn/ttcn')
-    require('codemirror/mode/ttcn-cfg/ttcn-cfg')
-    require('codemirror/mode/turtle/turtle')
-    require('codemirror/mode/twig/twig')
-    require('codemirror/mode/vb/vb')
-    require('codemirror/mode/vbscript/vbscript')
-    require('codemirror/mode/velocity/velocity')
-    require('codemirror/mode/verilog/verilog')
-    require('codemirror/mode/vhdl/vhdl')
-    require('codemirror/mode/vue/vue')
-    require('codemirror/mode/webidl/webidl')
-    require('codemirror/mode/xml/xml')
-    require('codemirror/mode/xquery/xquery')
-    require('codemirror/mode/yacas/yacas')
-    require('codemirror/mode/yaml/yaml')
-    require('codemirror/mode/yaml-frontmatter/yaml-frontmatter')
-    require('codemirror/mode/z80/z80')
     const { language } = this.props
-    this.setState({ render: true, selectedMode: language })
+    this.setState({ selectedMode: language })
   }
 
   languageChangeHandler = language => {
@@ -276,10 +273,9 @@ class CodeEditor extends Component {
   }
 
   render() {
-    const { render, selectedMode } = this.state
+    const { selectedMode } = this.state
     const { code, readonly, error } = this.props
     const { Option } = Select
-    if (!render) return null
     const { UnControlled } = CodeMirror
     return (
       <Fragment>
